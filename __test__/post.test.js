013cb825bdbf1719b769a86a7752368186c3e745
HodRZ <hodaifa.radwan@gmail.com>
2022-09-14 12:17:41 +0300

    Update populate function

--- __test__/post.test.js
+++ __test__/post.test.js
@@ -9,10 +9,11 @@ describe('/Post Route', () => {
             "image": "jest testing"
         }
         const addedPost = await request.post('/post').send(newPost);
-        const res = await request.get('/post');
-        expect(res.status).toEqual(200);
-        expect(res.body.post[0].title).toEqual('test from server.test');
         const { id } = addedPost.body
+        const res = await request.get(`/post/${id}?filter=comments`);
+        console.log(res.body)
+        expect(res.status).toEqual(200);
+        expect(res.body.title).toEqual('test from server.test');
         await request.delete(`/post/${id}`);
     });
     it('Get/:id, should get a route by id', async () => {
@@ -23,7 +24,7 @@ describe('/Post Route', () => {
         }
         const addedPost = await request.post('/post').send(newPost);
         const { id, img, title } = addedPost.body
-        const res = await request.get(`/post/${id}`);
+        const res = await request.get(`/post/${id}?filter=comments`);
         expect(res.status).toEqual(200);
         expect(res.body.title).toEqual(title);
         await request.delete(`/post/${id}`);
